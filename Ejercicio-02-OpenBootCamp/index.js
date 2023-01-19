c1a80f1b83da5fcbbe542cd9b7b24213b0c28412
Antonio Luis Martinez <antonillo_1995@hotmail.com>
2023-01-19 23:43:56 +0100

    Subo al repo el Ejercicio 02

---
+++ Ejercicio-02-OpenBootCamp/index.js
@@ -0,0 +1,30 @@
+// Mi nombre como "string"
+var nombre = "Antonio Luis Martínez"
+console.log(nombre)
+console.log(typeof nombre)
+
+// Mi edad como "number"
+var edad = 27
+console.log(edad)
+console.log(typeof edad)
+
+// Pregunta desarrollador (Y/N)
+if (true) {
+    console.log("Soy un desarrollador")
+    console.log(typeof true)
+} else {
+    console.log("No soy un desarrollador")
+}
+
+// Mi fecha de nacimiento
+var nacimiento = new Date(1995, 6, 24)
+console.log(nacimiento)
+console.log(typeof nacimiento)
+
+// Mi libro favorito
+const libro = {
+    titulo: "El valle de los lobos",
+    autor: "Laura Gallego García",
+    fecha: "2000",
+    url: "https://www.amazon.es/Cr%C3%B3nicas-Torre-Valle-los-Lobos/dp/8467539674/ref=asc_df_8467539674/?tag=googshopes-21&linkCode=df0&hvadid=195231592075&hvpos=&hvnetw=g&hvrand=5608356333914564825&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9047062&hvtargid=pla-125377223835&psc=1"
+}
